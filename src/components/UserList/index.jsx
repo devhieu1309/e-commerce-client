9ed79d9bd6a3925233096a31acbd69582bc3c361
buithamky <kyn827765@gmail.com>
2025-10-24 21:56:32 +0700

    Login & Register

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -3,8 +3,7 @@ import { useEffect, useState, useRef } from "react";
 import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
 import UserToolbar from "./UserToolbar";
 import UserDelete from "./UserDelete";
-import UserModal from "./UserModal";
-import UserView from "./UserView";
+import UserModal from "./UserModal";git
 import { getUserList, searchUser } from "../../services/userServices";
 
 function UserList() {
