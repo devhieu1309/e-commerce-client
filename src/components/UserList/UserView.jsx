02c904541e4488e3c424b49e6793a851c2487323
buithamky <kyn827765@gmail.com>
2025-10-24 22:13:04 +0700

    Xem Thông Tin Khách Hàng

--- src/components/UserList/UserView.jsx
+++ src/components/UserList/UserView.jsx
@@ -17,8 +17,8 @@ function UserView({ open, onCancel, user }) {
                 <Descriptions.Item label="Email">{user.email}</Descriptions.Item>
                 <Descriptions.Item label="Số điện thoại">{user.phone}</Descriptions.Item>
                 <Descriptions.Item label="Phân quyền">{user.role}</Descriptions.Item>
-                <Descriptions.Item label="Ngày tạo tài khoản">{user.createdAt}</Descriptions.Item>
-                <Descriptions.Item label="Địa chỉ">{user.address || "123 Nguyễn Trãi, Hà Nội"}</Descriptions.Item>
+                <Descriptions.Item label="Ngày tạo tài khoản">{user.created_at}</Descriptions.Item>
+                <Descriptions.Item label="Địa chỉ">{"123 Nguyễn Trãi, Hà Nội"}</Descriptions.Item>
                 <Descriptions.Item label="Trạng thái tài khoản">{user.status || "Đang hoạt động"}</Descriptions.Item>
                 <Descriptions.Item label="Số lượng đánh giá bài viết">{user.reviewCount || 12}</Descriptions.Item>
             </Descriptions>
