6e1de80729fb8279eaf60c0644e04d80edaf59a6
buithamky <kyn827765@gmail.com>
2025-11-18 18:48:41 +0700

    fix loi

--- src/services/addressServices.js
+++ src/services/addressServices.js
@@ -1,39 +1,30 @@
-import { get, post, edit, del, patch, put } from "../utils/request";
+// import { get, post, edit, del, patch, put } from "../utils/request";
 
-//  Lấy danh sách địa chỉ theo user_id (dạng query ?user_id=)
-export const getAddressesByUser = async (userId) => {
-  const result = await get(`addresses?user_id=${userId}`);
-  return result;
-};
-
-//  Thêm địa chỉ mới
-export const addAddress = async (option) => {
-  const result = await post(`addresses`, option);
-  return result;
-};
-
-//  Cập nhật địa chỉ
-// export const updateAddress = async (id, option) => {
-//   const result = await edit(`addresses/${id}`, option);
+// //  Lấy danh sách địa chỉ theo user_id (dạng query ?user_id=)
+// export const getAddressesByUser = async (userId) => {
+//   const result = await get(`addresses?user_id=${userId}`);
 //   return result;
 // };
-// export const updateAddress = async (id, option) => {
-//   const result = await patch(`addresses/${id}`, option);
+
+// //  Thêm địa chỉ mới
+// export const addAddress = async (option) => {
+//   const result = await post(`addresses`, option);
 //   return result;
 // };
-export const updateAddress = async (id, option) => {
-  const result = await put(`addresses/${id}`, option);
-  return result;
-};
 
-//  Xóa địa chỉ
-export const deleteAddress = async (id) => {
-  const result = await del(`addresses/${id}`);
-  return result;
-};
+// export const updateAddress = async (id, option) => {
+//   const result = await put(`addresses/${id}`, option);
+//   return result;
+// };
 
-//  Đặt địa chỉ mặc định
-export const setDefaultAddress = async (id) => {
-  const result = await patch(`addresses/${id}/default`);
-  return result;
-};
+// //  Xóa địa chỉ
+// export const deleteAddress = async (id) => {
+//   const result = await del(`addresses/${id}`);
+//   return result;
+// };
+
+// //  Đặt địa chỉ mặc định
+// export const setDefaultAddress = async (id) => {
+//   const result = await patch(`addresses/${id}/default`);
+//   return result;
+// };
